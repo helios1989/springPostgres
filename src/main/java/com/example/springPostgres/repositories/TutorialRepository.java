54ceb27c0dc98630ecba238c36f3c2ef0253520f
helios <vergel>
2021-06-21 09:59:11 +1200

    initial commit

---
+++ src/main/java/com/example/springPostgres/repositories/TutorialRepository.java
@@ -0,0 +1,13 @@
+package com.example.springPostgres.repositories;
+
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.springPostgres.model.Tutorial;
+public interface TutorialRepository extends JpaRepository<Tutorial, Long> {
+  List<Tutorial> findByPublished(boolean published);
+
+  List<Tutorial> findByTitleContaining(String title);
+}
---
+++ src/main/java/com/example/springPostgres/util/TestUtil.java
@@ -0,0 +1,10 @@
+package com.example.springPostgres.util;
+
+import org.springframework.stereotype.Service;
+
+@Service	
+public class TestUtil {
+public String Hello() {
+	return "Print helloWORLD";
+}
+}
